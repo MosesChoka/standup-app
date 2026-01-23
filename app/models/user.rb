a1d1a4f485ea65ae606aa015bd43d52c315c4a0f
MC <chokamoses2@gmail.com>
2026-01-23 15:01:07 +0300

    updated navigation views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,19 @@
 class User < ApplicationRecord 
+  require "digest/md5"
   rolify  
   
   attr_accessor :role # ensures that when the permission file loads a User object, it has access to a role attribute, even though roles are managed through an association and a gem.
   belongs_to :account, optional: true # the optional is set to true because an account may not exist when a user is first created 
 
   
-  devise :invitable, :database_authenticatable, :registerable,
-    :recoverable, :rememberable, :validatable
+  devise :invitable, :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
-    has_many :team_memberships
-    has_many :teams, through: :team_memberships
+  has_many :team_memberships
+  has_many :teams, through: :team_memberships
+
+  def avatar_url(size = 45)
+    gravatar_id = Digest::MD5.hexdigest(email.downcase)
+    "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=identicon"
+  end
 
 end
